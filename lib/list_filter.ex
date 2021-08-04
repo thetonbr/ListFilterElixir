604c70574d6e33405b9c4b8e78ca5d35d869ec1c
Adalton Goncalves <adalton.goncalves@directfocus.com.br>
2021-08-04 13:55:36 -0300

    Use Pipe

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,15 +1,7 @@
 defmodule ListFilter do
   def call(list) do
-    filtered_numbers = filter_numbers(list)
-    odd_numbers(filtered_numbers)
-  end
-
-  defp filter_numbers(list) do
-    Enum.filter(list, fn x -> Integer.parse(x) != :error end)
-  end
-
-  defp odd_numbers(list) do
-    odds = Enum.filter(list, fn x -> rem(String.to_integer(x), 2) != 0 end)
-    Enum.count(odds)
+    list
+    |> Enum.filter(&(Integer.parse(&1) != :error))
+    |> Enum.count(&(rem(String.to_integer(&1), 2) != 0))
   end
 end
